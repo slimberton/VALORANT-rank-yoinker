5e0c20555b89ef28f252ee2b8aff64bae34bf909
slimberton <slimberton@gmail.com>
2023-11-23 17:45:12 -0500

    Python and config changes

--- updatescript.bat
+++ updatescript.bat
@@ -1,5 +1,46 @@
 @echo off
-robocopy %1 %2 /E /Move>NUL
-rd /s /q %1>NUL
-cd %2>NUL
-@vRY.exe
+setlocal enabledelayedexpansion
+
+REM Check if the correct number of arguments is provided
+IF "%~2"=="" (
+    echo Usage: script_name.bat source_directory target_directory
+    exit /b 1
+)
+
+REM Assigning the source and target directories to variables for better readability
+set "sourceDir=%~1"
+set "targetDir=%~2"
+
+REM Ensure that the source directory exists
+IF NOT EXIST "%sourceDir%" (
+    echo Source directory not found: %sourceDir%
+    exit /b 1
+)
+
+REM Ensure that the target directory exists, if not create it
+IF NOT EXIST "%targetDir%" (
+    echo Creating target directory: %targetDir%
+    mkdir "%targetDir%"
+)
+
+REM Copying and moving files from the source to the target directory
+robocopy "%sourceDir%" "%targetDir%" /E /Move >NUL
+
+REM Check if robocopy was successful
+IF NOT %ERRORLEVEL% LEQ 1 (
+    echo Error occurred during file transfer.
+    exit /b %ERRORLEVEL%
+)
+
+REM Removing the source directory
+rd /s /q "%sourceDir%" >NUL
+
+REM Changing to the target directory
+cd /d "%targetDir%" >NUL
+
+REM Starting the vRY executable
+start "" @vRY.exe
+
+REM End of the script
+endlocal
+exit /b 0
